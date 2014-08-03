d254b74e35d7191d701659d8d8adcc6adadc11ac
Matt Cotter <mcotter222@gmail.com>
2014-08-03 14:46:26 -0400

    Bugfix: LinkOrWarn
    No longer follow the target if the target was a symlink

--- func.sh
+++ func.sh
@@ -6,7 +6,7 @@ linkOrWarn() {
                 "differences, merge differences if necessary, and then delete."
     else
         echo -e "\tLinking $2 to $PWD/$1"
-        ln -fs $PWD/$1 $2
+        ln -fhs $PWD/$1 $2
     fi
 }
 
