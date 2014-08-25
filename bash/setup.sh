4819adb85067e6e67e876957adb16d4f84c960d0
Matt Cotter <mcotter222@gmail.com>
2014-08-24 22:05:19 -0500

    add basic bash_logout and sort lines

--- bash/setup.sh
+++ bash/setup.sh
@@ -4,9 +4,10 @@ echo "Setting up bash configurations:"
 
 . ./func.sh
 
-linkOrWarn bash/bash_profile ~/.bash_profile
-linkOrWarn bash/bashrc ~/.bashrc
-linkOrWarn bash/bash_prompt ~/.bash_prompt
 linkOrWarn bash/bash_aliases ~/.bash_aliases
 linkOrWarn bash/bash_functions ~/.bash_functions
+linkOrWarn bash/bash_logout ~/.bash_logout
+linkOrWarn bash/bash_profile ~/.bash_profile
+linkOrWarn bash/bash_prompt ~/.bash_prompt
 linkOrWarn bash/bash_scripts ~/.bash_scripts
+linkOrWarn bash/bashrc ~/.bashrc
--- bash/teardown.sh
+++ bash/teardown.sh
@@ -4,9 +4,10 @@ echo "Tearing down bash configurations:"
 
 . ./func.sh
 
-removeOrWarn ~/.bash_profile
-removeOrWarn ~/.bashrc
-removeOrWarn ~/.bash_prompt
 removeOrWarn ~/.bash_aliases
 removeOrWarn ~/.bash_functions
+removeOrWarn ~/.bash_logout
+removeOrWarn ~/.bash_profile
+removeOrWarn ~/.bash_prompt
 removeOrWarn ~/.bash_scripts
+removeOrWarn ~/.bashrc
